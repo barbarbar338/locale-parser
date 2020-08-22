e4dbf41ae4b81f1195f91a0a83536e28e92846a9
barbarbar338 <demirci.baris38@gmail.com>
2020-08-23 00:07:26 +0300

    🔥 Forgot some files and tests

--- tests/parse.test.ts
+++ tests/parse.test.ts
@@ -18,14 +18,19 @@ describe("I18n Parse / Get Test", () => {
         expect(parser.get("tr", "info", "introduce")).to.equal("Selam, ben barbarbar338! demirci.baris38@gmail.com'a e-posta atın!");
         expect(parser.get("lol", "info", "introduce")).to.equal("Hey, It's barbarbar338! Send e-mails to demirci.baris38@gmail.com");
     });
-    it("Get String With Arguments From Keys", () => {
+    it("Get Strings With Arguments From Keys", () => {
         expect(parser.get("en", "info", "withArgs", { result: "10" })).to.equal("5+5 equals 10");
         expect(parser.get("tr", "info", "withArgs", { result: "10" })).to.equal("5+5 10'a eşittir");
         expect(parser.get("lol", "info", "withArgs", { result: "10" })).to.equal("5+5 equals 10");
     });
-    it("Get String With Arguments And Constants From Keys", () => {
+    it("Get Strings With Arguments And Constants From Keys", () => {
         expect(parser.get("en", "info", "withArgsAndConstants", { result: "10" })).to.equal("barbarbar338 says: 5+5 equals 10");
         expect(parser.get("tr", "info", "withArgsAndConstants", { result: "10" })).to.equal("barbarbar338 diyor ki: 5+5 10'a eşittir");
         expect(parser.get("lol", "info", "withArgsAndConstants", { result: "10" })).to.equal("barbarbar338 says: 5+5 equals 10");
+    });
+    it("Get Strings From Another Section", () => {
+        expect(parser.get("en", "test", "test")).to.equal("this is a test");
+        expect(parser.get("tr", "test", "test")).to.equal("bu bir test");
+        expect(parser.get("lol", "test", "test")).to.equal("this is a test")
     });
 });
